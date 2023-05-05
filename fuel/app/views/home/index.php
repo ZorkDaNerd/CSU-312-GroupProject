40c195920818591db76cfcbf6df4eaffddfc9933
linobars <linobarrios90@gmail.com>
2023-05-04 22:19:47 -0600

    make home look better

--- fuel/app/views/home/index.php
+++ fuel/app/views/home/index.php
@@ -1,15 +1,23 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 	<body>
+		<h1 id ="header">Hello ! </h1>
+		<div id = "helpme"> 
 		<p>
 			Welcome to Necronomicompany, your first and best stop for purchasing your very own Necronomicon and associated products 
 			for all your ritual, dark sacrifice, crafting, or cooking needs!
 		</p>
+
+		<h2> Our Mission</h2>
 		<p>
-			We here at Necronomicompany sell from only the finest of suppliers and are the only Nyarlathotep-approved sellers of the 
+			Our mission at Necronomicompany is to help our custmers for whatever they need.  We here at Necronomicompany sell from only the finest of suppliers and are the only Nyarlathotep-approved sellers of the 
 			Necronomicons!*
 		</p>
 		<p style = "font-size:70%">
 			*And by that we mean we were the only ones dumb enough to summon him over and over until he said yes. RIP 1,576 interns.
 		</p>
+		</div>
 	</body>
 </html>
